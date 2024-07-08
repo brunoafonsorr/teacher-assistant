26f1e2cb3d8c67315a6bc60141b8f6fa61a7fdf0
brunoafonsorr <bruno.afonsorr.programador@gmail.com>
2024-07-07 21:37:06 -0300

    login-page started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "400"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={poppins.className} id="root">{children}</body>
     </html>
   );
 }
